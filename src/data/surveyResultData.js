a25f9d1fff15c6205cb447d86263937ec43e9636
seongyu <seongyu1066@naver.com>
2023-08-11 04:55:09 +0900

    refact: 설문조사 페이지에 필요한 데이터 구조 변경

---
+++ src/data/surveyResultData.js
@@ -0,0 +1,53 @@
+const surveyResultData = {
+  영구: {
+    id: 0,
+    title: "영구",
+    hashtag: "#연구해요 #몰입중 #하나만_판다",
+    img: "/img/Survey7.svg",
+  },
+  소심: {
+    id: 1,
+    title: "소심",
+    hashtag: "#소심하지만_적극적 #인터넷인싸",
+    img: "/img/Survey4.svg",
+  },
+  유익: {
+    id: 2,
+    title: "유익",
+    hashtag: "#최고효율 #유익한자료 #알쓸신잡",
+    img: "/img/Survey8.svg",
+  },
+  고독: {
+    id: 3,
+    title: "고독",
+    hashtag: "#나만의삶 #마이웨이 #집중",
+    img: "/img/Survey9.svg",
+  },
+  소통: {
+    id: 4,
+    title: "소통",
+    hashtag: "#1디저트_3수다 #말로공부 #대화",
+    img: "/img/Survey5.svg",
+  },
+  북적: {
+    id: 5,
+    title: "북적",
+    hashtag: "#왁자지껄 #뮤직이즈_마이라이프",
+    img: "/img/Survey3.svg",
+    req: "BJ",
+  },
+  집중: {
+    id: 6,
+    title: "집중",
+    hashtag: "#열쩡열쩡열쩡! #한_우물만 #초집중",
+    img: "/img/Survey10.svg",
+  },
+  한산: {
+    id: 7,
+    title: "한산",
+    hashtag: "#같이_공부만 #백색소음 #조용한",
+    img: "/img/Survey1.svg",
+  },
+};
+
+export default surveyResultData;
